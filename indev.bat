e0a935b4267d4443902052532c75210c543536dd
tsssss <40420318+fried-chicken-404@users.noreply.github.com>
2021-01-20 13:46:11 +0700

    edit as you need

---
+++ indev.bat
@@ -0,0 +1,157 @@
+@echo off
+:start_screen
+cls
+rem PrintBoxAt 1 1 24 80 2
+rem ShadeBoxAt 3 39 1 1 3
+rem ShadeBoxAt 4 38 1 1 3
+rem ShadeBoxAt 5 37 3 1 3
+rem ShadeBoxAt 4 36 1 1 3
+rem ShadeBoxAt 3 35 1 1 3
+rem ShadeBoxAt 4 42 1 1 3
+rem ShadeBoxAt 3 43 1 4 3
+rem ShadeBoxAt 5 43 1 3 3
+rem ShadeBoxAt 6 46 1 1 3
+rem ShadeBoxAt 7 42 1 4 3
+rem ShadeBoxAt 3 48 5 1 3
+rem ShadeBoxAt 3 49 1 2 3
+rem ShadeBoxAt 3 51 1 1 3
+rem ShadeBoxAt 4 52 1 1 3
+rem ShadeBoxAt 5 49 1 3 3
+rem ShadeBoxAt 4 54 4 1 3
+rem ShadeBoxAt 3 55 1 3 3
+rem ShadeBoxAt 4 58 4 1 3
+rem ShadeBoxAt 5 55 1 3 3
+rem ShadeBoxAt 7 61 1 4 3
+rem ShadeBoxAt 4 60 3 1 3
+rem ShadeBoxAt 3 61 1 4 3
+rem ShadeBoxAt 3 66 1 5 3
+rem ShadeBoxAt 4 66 4 1 3
+rem ShadeBoxAt 7 67 1 4 3
+rem ShadeBoxAt 5 67 1 3 3
+rem ShadeBoxAt 3 30 1 4 3
+rem ShadeBoxAt 4 29 3 1 3
+rem ShadeBoxAt 7 30 1 4 3
+rem ShadeBoxAt 5 32 1 2 3
+rem ShadeBoxAt 6 33 1 1 3
+rem ShadeBoxAt 4 27 3 1 3
+rem ShadeBoxAt 7 24 1 3 3
+rem ShadeBoxAt 4 23 3 1 3
+rem ShadeBoxAt 3 24 1 3 3
+rem ShadeBoxAt 4 21 1 1 3
+rem ShadeBoxAt 5 18 1 3 3
+rem ShadeBoxAt 3 17 1 4 3
+rem ShadeBoxAt 4 17 4 1 3
+rem ShadeBoxAt 6 19 1 1 3
+rem ShadeBoxAt 7 20 1 1 3
+rem ShadeBoxAt 3 11 1 5 3
+rem ShadeBoxAt 4 13 4 1 3
+rem ChangeColor 0 7
+rem ShadeBoxAt 10 40 1 1 1
+rem ShadeBoxAt 11 40 1 1 1
+rem ShadeBoxAt 12 41 2 1 1
+rem ShadeBoxAt 14 42 2 1 1
+rem ShadeBoxAt 16 43 1 1 1
+rem ShadeBoxAt 17 44 1 1 1
+rem ShadeBoxAt 17 45 1 1 1
+rem ShadeBoxAt 12 39 2 1 1
+rem ShadeBoxAt 14 38 2 1 1
+rem ShadeBoxAt 16 37 1 1 1
+rem ShadeBoxAt 17 35 1 2 1
+rem ShadeBoxAt 18 34 1 1 1
+rem ShadeBoxAt 18 46 1 1 1
+rem ShadeBoxAt 19 45 1 1 1
+rem ShadeBoxAt 19 46 1 1 1
+rem ShadeBoxAt 19 34 1 5 1
+rem ShadeBoxAt 19 43 1 2 1
+rem ShadeBoxAt 19 42 1 1 1
+rem ShadeBoxAt 17 40 1 1 1
+rem ShadeBoxAt 18 41 1 1 1
+rem ShadeBoxAt 18 39 1 1 1
+rem ChangeColor 11 15
+rem ShadeBoxAt 12 40 2 1 1
+rem ChangeColor 7 15
+rem ShadeBoxAt 14 39 1 3 2
+rem ShadeBoxAt 15 39 1 3 2
+rem ShadeBoxAt 16 38 1 5 2
+rem ShadeBoxAt 17 37 1 3 2
+rem ShadeBoxAt 17 41 1 3 2
+rem ShadeBoxAt 18 42 1 4 2
+rem ShadeBoxAt 18 35 1 4 2
+rem ShadeBoxAt 14 44 3 1 3
+rem ShadeBoxAt 15 45 2 1 3
+rem ShadeBoxAt 14 45 1 1 3
+rem ShadeBoxAt 13 44 1 1 3
+rem ShadeBoxAt 13 36 4 1 3
+rem ShadeBoxAt 14 35 3 1 3
+rem ChangeColor 14 12
+rem ShadeBoxAt 20 43 1 3 1
+rem ShadeBoxAt 21 46 1 1 1
+rem ShadeBoxAt 21 42 1 1 1
+rem ShadeBoxAt 20 35 1 3 1
+rem ShadeBoxAt 21 38 1 1 1
+rem ShadeBoxAt 21 34 1 1 1
+rem ShadeBoxAt 21 35 1 3 2
+rem ShadeBoxAt 22 36 1 1 2
+rem ShadeBoxAt 21 43 1 3 2
+rem ShadeBoxAt 22 44 1 1 2
+rem ShadeBoxAt 22 45 1 1 3
+rem ShadeBoxAt 22 43 1 1 3
+rem ShadeBoxAt 23 44 1 1 3
+rem ShadeBoxAt 22 37 1 1 3
+rem ShadeBoxAt 23 36 1 1 3
+rem ShadeBoxAt 22 35 1 1 3
+rem ChangeColor 15 15
+rem ShadeBoxAt 12 58 1 1 3
+rem ShadeBoxAt 13 70 1 1 3
+rem ShadeBoxAt 8 75 1 1 3
+rem ShadeBoxAt 12 9 1 1 3
+rem ShadeBoxAt 19 21 1 1 3
+rem ShadeBoxAt 5 25 1 1 3
+rem ShadeBoxAt 18 64 1 1 3
+rem ShadeBoxAt 15 56 1 1 3
+rem ShadeBoxAt 11 63 1 1 3
+rem ShadeBoxAt 18 71 1 1 3
+rem ShadeBoxAt 11 24 1 1 3
+rem ShadeBoxAt 18 26 1 1 3
+rem ShadeBoxAt 11 29 1 1 3
+rem ShadeBoxAt 9 38 1 1 3
+rem ShadeBoxAt 10 51 1 1 3
+rem ShadeBoxAt 21 56 1 1 3
+rem ShadeBoxAt 15 63 1 1 3
+rem ShadeBoxAt 16 77 1 1 3
+rem ShadeBoxAt 22 75 1 1 3
+rem ChangeColor 15 6
+rem ShadeBoxAt 11 51 1 1 3
+rem ChangeColor 0 7
+rem ShadeBoxAt 19 47 1 1 1
+rem ShadeBoxAt 19 33 1 1 1
+rem ChangeColor 11 0
+rem ShadeBoxAt 8 44 1 1 4
+rem ShadeBoxAt 9 36 1 1 4
+rem ShadeBoxAt 9 44 1 1 3
+rem ShadeBoxAt 10 36 1 1 3
+rem ShadeBoxAt 10 44 1 1 2
+rem ShadeBoxAt 11 36 1 1 2
+rem ShadeBoxAt 11 44 1 1 1
+rem ShadeBoxAt 12 36 1 1 1
+rem ShadeBoxAt 4 44 1 1 1
+rem ShadeBoxAt 2 44 1 1 3
+rem ShadeBoxAt 5 36 1 1 1
+rem ShadeBoxAt 3 36 1 1 3
+rem ShadeBoxAt 2 36 1 1 4
+rem PrintBoxAt 13 25 3 7 1
+rem PrintBoxAt 13 48 3 12 1
+rem ChangeColor 15 0
+rem ShadeBoxAt 16 56 1 1 4
+rem ChangeColor 11 0
+rem PrintBoxAt 13 48 3 7 1
+rem ChangeColor 0 0
+rem ShadeBoxAt 13 55 3 5 4
+
+rem Locate 25 1
+pause
+
+
+
+rem Locate 24 1
+pause >nul
